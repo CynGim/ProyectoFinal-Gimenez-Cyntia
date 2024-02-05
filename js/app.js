51341ac5b146f561e5829b7eeedf6da5f6f34d73
Cyntia <cyntiasgimenez@gmail.com>
2024-02-05 01:10:29 +0100

    img ruta link

--- js/app.js
+++ js/app.js
@@ -1,80 +1,14 @@
 //metodo para recorrer los productos con funcion array mediante forEach
 const shopContent = document.getElementById("shopContent");
 const verCarrito = document.getElementById("verCarrito");
-const modalContainer =document.getElementById("modal-container");
+const modalContainer = document.getElementById("modal-container");
 const showAlert = document.getElementById("showAlert");
 const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 //obtengo lo que tengo guardado en el localStorage
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-//funcion asincrona de asyncawait mediante fetch mencionando la ruta de los productos de data.json generando promesas
-/*const getProducts = async () => {
-    const response = await fetch("/data.json");
-    const data = await response.json("");
-    console.log(data);*/
-    
-    /*const getProducts = async () => {
-    const response = await fetch("../data/datos.json");
-    const data = await response.json("");
-    console.log(data);
-
-    fetch("../data/datos.json")
-    .then(response => response.json())
-    .then(datos => {
-        let shopContent = document.getElementById("shop-content");
-        datos.forEach((item) => {
-            let div = document.createElement("div");
-            
-        })
-        });
-    
-    data.forEach((product)=> {
-        let content = document.createElement("div");
-        content.className = "card";
-        content.innerHTML = `
-        <img src="${product.imagen}">
-        <h3>${product.nombre}</h3>
-        <p class="price">€${product.precio}</p></div>
-        `;
-    
-        shopContent.append(content);
-    
-        let comprar = document.createElement("button");
-        comprar.innerText = "Agregar al carrito";
-        comprar.className = "Agregar al carrito";
-    
-        content.append(comprar);
-       
-    //creo evento para detectar accion (agregando objetos al carrito) y aplico funcion para que detecte los productos que se repiten mediante some devolviendo valor true o false
-    
-    comprar.addEventListener("click", () => { 
-     const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-        
-    //creo condicion sumando la cantidad si el producto es igual
-        if (repeat){
-            carrito.map((prod) => {
-             if (prod.id === product.id) {
-             prod.cantidad++; 
-             } 
-           });
-           
-        } else {
-            carrito.push({
-                id : product.id,
-                imagen: product.imagen,
-                nombre: product.nombre,
-                precio: product.precio,
-                cantidad: product.cantidad,
-            });
-        console.log(carrito);
-        console.log(carrito.length);
-        carritoCounter(); 
-        saveLocal();
-        }
-       }); 
-    });
-};*/
+//funcion asincrona de async por medio de fetch mencionando la ruta de los productos de data.json generando promesas
 
 const getProducts = async () => {
     fetch("data/datos.json")
@@ -119,11 +53,14 @@ const getProducts = async () => {
                             precio: product.precio,
                             cantidad: product.cantidad,
                         });
-                        console.log(carrito);
-                        console.log(carrito.length);
-                        carritoCounter();
-                        saveLocal();
                     }
+                    Toastify({
+                        text: "Producto Agregado al Carrito",
+                        duration: 3000,
+                        position: "center"
+                    }).showToast();
+                    carritoCounter();
+                    saveLocal();
                 });
             });
         });
@@ -133,5 +70,5 @@ getProducts();
 
 //envio y guardo informacion mediante localStorage y JSON
 const saveLocal = () => {
-localStorage.setItem("carrito", JSON.stringify(carrito));
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 };
